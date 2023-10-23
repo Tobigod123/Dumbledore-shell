c07c7fe49da6aff9a49b92b2edbbc2aee4188362
Tobigod123 <134499497+Tobigod123@users.noreply.github.com>
2023-10-23 20:29:06 +0530

    Update __init__.py

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -1,6 +1,6 @@
 from os import environ
 
-from telegram.ext.requesthandler.defaults import Defaults
+from telegram.ext.defaults import Defaults
 
 from logging import getLogger, FileHandler, StreamHandler, INFO, basicConfig
 
